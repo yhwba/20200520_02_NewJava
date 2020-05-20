bf43a766fdf6d2a84146eb6bbd67e925144818d2
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-20 16:56:17 +0900

    커튼 클릭 이벤트 기초 작업

--- app/src/main/java/kr/co/yhw/a20200520_02_newjava/MainActivity.java
+++ app/src/main/java/kr/co/yhw/a20200520_02_newjava/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
-//        binding.
+        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("메인화면","확인버튼클릭");
+            }
+        });
     }
 }
