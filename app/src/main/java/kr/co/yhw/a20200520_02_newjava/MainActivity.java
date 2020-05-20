714479743811d281e7aa69a83e0f809179f215d5
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-20 15:59:26 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/yhw/a20200520_02_newjava/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.co.yhw.a20200520_02_newjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
