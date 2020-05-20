81b8c1e70e2eeaf78e6b5b6cce0fce38fe68248d
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-20 16:10:45 +0900

    멤버 변수에 연결

--- app/src/main/java/kr/co/yhw/a20200520_02_newjava/MainActivity.java
+++ app/src/main/java/kr/co/yhw/a20200520_02_newjava/MainActivity.java
@@ -3,12 +3,23 @@ package kr.co.yhw.a20200520_02_newjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText contentEdt;
+    Button okButton;
+    TextView contentTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contentEdt = findViewById(R.id.contentEdt);
+        okButton = findViewById(R.id.okBtn);
+        contentTxt =findViewById(R.id.contentTxt);
     }
 }
