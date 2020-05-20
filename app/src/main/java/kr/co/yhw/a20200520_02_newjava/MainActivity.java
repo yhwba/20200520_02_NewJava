7bb69909c9468ac99f3dc6aed6267964708768d7
yhwba <59465345+yhwba@users.noreply.github.com>
2020-05-20 16:39:01 +0900

    데이터 바인딩 셋팅

--- app/src/main/java/kr/co/yhw/a20200520_02_newjava/MainActivity.java
+++ app/src/main/java/kr/co/yhw/a20200520_02_newjava/MainActivity.java
@@ -1,25 +1,24 @@
 package kr.co.yhw.a20200520_02_newjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import kr.co.yhw.a20200520_02_newjava.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText contentEdt;
-    Button okButton;
-    TextView contentTxt;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
 
-        contentEdt = findViewById(R.id.contentEdt);
-        okButton = findViewById(R.id.okBtn);
-        contentTxt =findViewById(R.id.contentTxt);
+//        binding.
     }
 }
